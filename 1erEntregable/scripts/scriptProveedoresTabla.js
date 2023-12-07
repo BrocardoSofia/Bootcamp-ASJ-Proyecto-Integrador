ee4a5493827dc55c037d9f43d1b9bf2bb656a932
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-12-07 20:30:45 -0300

    Se ven todos los datos de llos proveedores cargados

--- 1erEntregable/scripts/scriptProveedoresTabla.js
+++ 1erEntregable/scripts/scriptProveedoresTabla.js
@@ -8,9 +8,9 @@ function cargarProveedoresTabla(){
     for(let i=0; i<arrayProveedores.length; i++){
         
         let proovedor = crearTdProveedor(arrayProveedores[i]); console.log(proovedor);
-        if(contadorTD == 4)
+        if(contadorTD == 3)
         {
-            //si el contador es igual a 4 tengo que cargar la fila anterior y crear una nueva
+            //si el contador es igual a 3 tengo que cargar la fila anterior y crear una nueva
             tabla.appendChild(row);
             row = document.createElement("section");
             row.className="row";
@@ -26,10 +26,10 @@ function cargarProveedoresTabla(){
 //devuelve un td
 function crearTdProveedor(proveedor){
     let col = document.createElement("div");
-    col.className = "col-md-3";
+    col.className = "col-md-4";
 
     let card = document.createElement("div");
-    card.className = "card";
+    card.className = "card text-dark bg-light";
     card.style.border = "solid rgb(171, 171, 171) 0.05em";
     card.style.borderRadius = "1em";
     card.style.padding = "1em";
@@ -39,18 +39,64 @@ function crearTdProveedor(proveedor){
     card.className = "card-body";
 
     let titulo = document.createElement("h5");
-    titulo.innerHTML = "Codigo: "+proveedor.codigo;
+    titulo.appendChild(document.createTextNode("Codigo: "+proveedor.codigo));
 
     let razonSocial = document.createElement("p");
-    razonSocial.innerHTML = "Razon Social: "+proveedor.razonSocial;
+    razonSocial.appendChild(document.createTextNode("Razon Social: "+proveedor.razonSocial));
 
     let rubro = document.createElement("p");
-    rubro.innerHTML = "Rubro: "+proveedor.rubro;
+    rubro.appendChild(document.createTextNode("Rubro: "+proveedor.rubro));
 
     let email = document.createElement("p");
-    email.innerHTML = "Email: "+proveedor.email;
+    email.appendChild(document.createTextNode("Email: "+proveedor.email));
 
     let line = document.createElement("hr");
+
+    let tituloDireccion = document.createElement("h5");
+    tituloDireccion.appendChild(document.createTextNode("Dirección"));
+
+    let calle = document.createElement("p");
+    calle.appendChild(document.createTextNode("Calle: "+proveedor.direccion.calle));
+
+    let codigoPostal = document.createElement("p");
+    codigoPostal.appendChild(document.createTextNode("Codigo Postal: "+proveedor.direccion.codigoPostal));
+
+    let localidad = document.createElement("p");
+    localidad.appendChild(document.createTextNode("Localidad: "+proveedor.direccion.localidad));
+
+    let provincia = document.createElement("p");
+    provincia.appendChild(document.createTextNode("Provincia: "+proveedor.direccion.provincia));
+
+    let pais = document.createElement("p");
+    pais.appendChild(document.createTextNode("Pais: "+proveedor.direccion.pais));
+
+    let datosFiscales = document.createElement("h5");
+    datosFiscales.appendChild(document.createTextNode("Datos Fiscales"));
+
+    let CUIT = document.createElement("p");
+    CUIT.appendChild(document.createTextNode("CUIT: "+proveedor.datosFiscales.CUIT));
+
+    let condicionIVA = document.createElement("p");
+    condicionIVA.appendChild(document.createTextNode("Condicion de IVA: "+
+     obtenerCondicionIva(proveedor.datosFiscales.condicionIVA)));
+
+    let datosContacto = document.createElement("h5");
+    datosContacto.appendChild(document.createTextNode("Datos de Contacto"));
+
+    let nombre = document.createElement("p");
+    nombre.appendChild(document.createTextNode("Nombre: "+proveedor.datosContacto.nombre));
+
+    let apellido = document.createElement("p");
+    apellido.appendChild(document.createTextNode("Apellido: "+proveedor.datosContacto.apellido));
+
+    let telefono = document.createElement("p");
+    telefono.appendChild(document.createTextNode("Telefono: "+proveedor.datosContacto.telefono));
+
+    let emailC = document.createElement("p");
+    emailC.appendChild(document.createTextNode("Email: "+proveedor.datosContacto.email));
+
+    let rol = document.createElement("p");
+    rol.appendChild(document.createTextNode("Rol: "+proveedor.datosContacto.rol));
 
     //agrego los elementos al card
     cardBody.appendChild(titulo);
@@ -58,11 +104,70 @@ function crearTdProveedor(proveedor){
     cardBody.appendChild(razonSocial);
     cardBody.appendChild(rubro);
     cardBody.appendChild(email);
+    line = document.createElement("hr");
+    cardBody.appendChild(line);
+    cardBody.appendChild(tituloDireccion);
+    cardBody.appendChild(calle);
+    cardBody.appendChild(codigoPostal);
+    cardBody.appendChild(localidad);
+    cardBody.appendChild(provincia);
+    cardBody.appendChild(pais);
+    line = document.createElement("hr");
+    cardBody.appendChild(line);
+    cardBody.appendChild(datosFiscales);
+    cardBody.appendChild(CUIT);
+    cardBody.appendChild(condicionIVA);
+    line = document.createElement("hr");
+    cardBody.appendChild(line);
+    cardBody.appendChild(datosContacto);
+    cardBody.appendChild(nombre);
+    cardBody.appendChild(apellido);
+    cardBody.appendChild(telefono);
+    cardBody.appendChild(emailC);
+    cardBody.appendChild(rol);
 
     card.appendChild(cardBody);
     col.appendChild(card);
 
     return col;
+}
+
+function obtenerCondicionIva(num){
+    let condicion = "";
+
+    switch(num)
+    {
+        case "1":
+            condicion = "IVA Responsable Inscripto";
+            break;
+        
+        case "2":
+            condicion = "IVA Responsable no Inscripto";
+            break;
+
+        case "3":
+            condicion = "IVA no Responsable";
+            break;
+
+        case "4":
+            condicion = "IVA Sujeto Exento";
+            break;
+
+        case "5":
+            condicion = "Responsable Monotributo";
+            break;
+
+        case "6":
+            condicion = "Sujeto no Categorizado";
+            break;
+
+        case "7":
+            condicion = "Proveedor del Exterior";
+            break;
+
+    }
+
+    return condicion;
 }
 
 /*
