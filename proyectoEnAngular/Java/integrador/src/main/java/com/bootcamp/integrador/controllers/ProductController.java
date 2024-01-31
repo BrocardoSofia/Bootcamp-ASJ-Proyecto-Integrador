2d8c98af8a71b656d42270c0180bbb00457b27d2
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-01-31 09:40:02 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package com.bootcamp.integrador.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bootcamp.integrador.models.ProductModel;
 import com.bootcamp.integrador.services.ProductService;
 
 @RestController
@@ -15,10 +20,31 @@ public class ProductController {
 	ProductService productService;
 	
 	//obtener todos los productos
+	@GetMapping()
+	public Page<ProductModel> getProducts(Pageable pageable, 
+									@RequestParam(required = false, defaultValue = "") String productName,
+									@RequestParam(required = false, defaultValue = "") String codeSKU,
+									@RequestParam(required = false, defaultValue = "-1") int supplierId){
+		return productService.getProducts(pageable, supplierId, codeSKU, productName);
+	}
 	
 	//obtener productos activos
+	@GetMapping("/active")
+	public Page<ProductModel> getActiveProducts(Pageable pageable, 
+									@RequestParam(required = false, defaultValue = "") String productName,
+									@RequestParam(required = false, defaultValue = "") String codeSKU,
+									@RequestParam(required = false, defaultValue = "-1") int supplierId){
+		return productService.getActiveProducts(pageable, supplierId, codeSKU, productName);
+	}
 	
 	//obtener productos eliminados
+	@GetMapping("/deleted")
+	public Page<ProductModel> getDeletedProducts(Pageable pageable, 
+									@RequestParam(required = false, defaultValue = "") String productName,
+									@RequestParam(required = false, defaultValue = "") String codeSKU,
+									@RequestParam(required = false, defaultValue = "-1") int supplierId){
+		return productService.getDeletedProducts(pageable, supplierId, codeSKU, productName);
+	}
 	
 	//get por Id
 	
