88330d8eef16b42f450b1e1541046f15b7a157e9
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-01-31 09:32:34 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
@@ -1,5 +1,33 @@
 package com.bootcamp.integrador.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bootcamp.integrador.services.ProductService;
+
+@RestController
+@RequestMapping("/products") //localhost:8080/products
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
+	@Autowired
+	ProductService productService;
+	
+	//obtener todos los productos
+	
+	//obtener productos activos
+	
+	//obtener productos eliminados
+	
+	//get por Id
+	
+	//cargar nuevo producto
+	
+	//eliminar producto
+	
+	//reinsertar producto
+	
+	//modificar producto
 
 }
