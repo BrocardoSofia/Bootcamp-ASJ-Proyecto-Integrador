9ca70dd423b1d9e1404ec12b210cad7b07d6bb64
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-01-31 09:50:10 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/ProductController.java
@@ -8,8 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,8 +68,24 @@ public class ProductController {
 	}
 	
 	//cargar nuevo producto
+	@PostMapping()
+	public ResponseEntity<ProductModel> addProduct(@RequestBody ProductModel product){
+		ProductModel productAdded = productService.addProduct(product);
+		
+		if(productAdded == null) {
+			return new ResponseEntity<>(productAdded, HttpStatus.CONFLICT);
+		}else {
+			return new ResponseEntity<>(productAdded, HttpStatus.CREATED);
+		}
+	}
 	
 	//eliminar producto
+	@DeleteMapping()
+	public ResponseEntity<ProductModel> deleteProduct(@RequestBody ProductModel product){
+		ProductModel productDeleted = productService.deleteProduct(product);
+		
+		return new ResponseEntity<>(productDeleted, HttpStatus.OK);
+	}
 	
 	//reinsertar producto
 	
