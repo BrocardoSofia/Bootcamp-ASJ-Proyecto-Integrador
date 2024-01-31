bcb0a1a611e5514e21b3719bf35ef76bdced09b2
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-01-30 22:13:25 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/ProductService.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/ProductService.java
@@ -1,5 +1,28 @@
 package com.bootcamp.integrador.services;
 
-public class ProductService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.bootcamp.integrador.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	ProductRepository productRepository;
+	
+	//obtener productos
+	
+	//obtener productos activos
+	
+	//obtener productos eliminados
+	
+	//obtener un producto segun su id
+	
+	//insertar producto
+	
+	//eliminar producto
+	
+	//reingresar producto
+	
+	//modificar producto
 }
