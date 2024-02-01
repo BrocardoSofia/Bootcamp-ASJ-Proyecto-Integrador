f7e24944ddbdb4cb56f24360646946c322752dd0
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-01 14:52:31 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/PurchaseOrderController.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/controllers/PurchaseOrderController.java
@@ -1,5 +1,13 @@
 package com.bootcamp.integrador.controllers;
 
 public class PurchaseOrderController {
-
+	//obtener todas las ordenes de compra
+	
+	//obtener ordenes de compra por proveedor
+	
+	//obtener ordenes de compra por usuario
+	
+	//modificar orden de compra
+	
+	//agregar orden de compra
 }
--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/PurchaseOrderService.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/PurchaseOrderService.java
@@ -1,5 +1,35 @@
 package com.bootcamp.integrador.services;
 
-public class PurchaseOrderService {
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import com.bootcamp.integrador.models.PurchaseOrderModel;
+import com.bootcamp.integrador.repositories.PurchaseOrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+
+@Service
+public class PurchaseOrderService {
+	@Autowired
+	PurchaseOrderRepository purchaseOrderRepository;
+	
+	//obtener todas las ordenes de compra
+	Page<PurchaseOrderModel> getPurchaseOrders(Pageable pageable){
+		return purchaseOrderRepository.findAll(pageable);
+	}
+	
+	//obtener ordenes de compra por proveedor
+	Page<PurchaseOrderModel> getPurchaseOrdersBySupplierId(Pageable pageable, int supplierId){
+		return purchaseOrderRepository.findAllBySupplierId(pageable, supplierId);
+	}
+	
+	//obtener ordenes de compra por usuario
+	Page<PurchaseOrderModel> getPurchaseOrdersByUserId(Pageable pageable, int userId){
+		return purchaseOrderRepository.findAllByCreatedBy(pageable, userId);
+	}
+	
+	//modificar orden de compra
+	
+	//agregar orden de compra
 }
