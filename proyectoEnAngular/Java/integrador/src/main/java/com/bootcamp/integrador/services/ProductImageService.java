40bf360cbf14e8438babfc65d8c1a53d0ac5eb90
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-01 10:17:25 -0300

    update

--- proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/ProductImageService.java
+++ proyectoEnAngular/Java/integrador/src/main/java/com/bootcamp/integrador/services/ProductImageService.java
@@ -1,5 +1,6 @@
 package com.bootcamp.integrador.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,26 @@ public class ProductImageService {
 		
         return productImageSaved;
     }
+	
+	public ProductImageModel deleteProductImage(int id) {
+		ProductImageModel productImageDeleted = productImageRepository.findById(id).get();
+		
+		if(productImageDeleted != null) {
+			productImageDeleted.setDeletedAt(LocalDateTime.now());
+			productImageDeleted = productImageRepository.save(productImageDeleted);
+		}
+		
+		return productImageDeleted;
+	}
+	
+	public ProductImageModel updateProductImage(ProductImageModel productImage) {
+		ProductImageModel productImageUpdated = productImageRepository.findById(productImage.getId()).get();
+		
+		if(productImageUpdated != null) {
+			productImageUpdated.setImageURL(productImage.getImageURL());
+			productImageUpdated = productImageRepository.save(productImageUpdated);
+		}
+		
+		return productImageUpdated;
+	}
 }
