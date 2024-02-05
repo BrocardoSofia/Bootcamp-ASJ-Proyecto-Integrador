4da55e14f86e5fd8fbbada85b5896a2546d4e625
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-05 17:36:38 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/components/admin/new-supplier-categories/new-supplier-categories.component.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/components/admin/new-supplier-categories/new-supplier-categories.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './new-supplier-categories.component.css'
 })
 export class NewSupplierCategoriesComponent {
-
+  edit: boolean = false;
+  supplierCategory: string = '';
 }
