4f71ad6b35fe46d6c467ea6683cdb5bc56edd979
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-06 15:08:46 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/services/product-categories.service.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/services/product-categories.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductCategoriesService {
+  private url: string = 'http://localhost:8080/product-categories';
 
   constructor() { }
 }
