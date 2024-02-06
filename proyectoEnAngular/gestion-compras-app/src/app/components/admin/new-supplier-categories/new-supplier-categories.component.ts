0914ea65666984701283742beb42a799b89b6a39
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-06 09:20:25 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/components/admin/new-supplier-categories/new-supplier-categories.component.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/components/admin/new-supplier-categories/new-supplier-categories.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-supplier-categories',
   templateUrl: './new-supplier-categories.component.html',
   styleUrl: './new-supplier-categories.component.css'
 })
-export class NewSupplierCategoriesComponent {
+export class NewSupplierCategoriesComponent implements OnInit{
   edit: boolean = false;
   supplierCategory: string = '';
+  submitForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private activeRoute: ActivatedRoute,
+              private router: Router){}
+
+  ngOnInit(): void {
+    this.submitForm = this.fb.group({
+      category: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]]
+    });
+  }
+
+  submitCategory(){
+    
+  }
 }
