8e5e2d4650a82fbc65edafa16184ed3d8e702693
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-06 15:12:01 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/services/product-categories.service.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/services/product-categories.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProductCategory } from '../models/product-category';
+import { SupplierCategoriesService } from './supplier-categories.service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,19 @@ import { Injectable } from '@angular/core';
 export class ProductCategoriesService {
   private url: string = 'http://localhost:8080/product-categories';
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private supplierCategoryService: SupplierCategoriesService) { }
+
+  public inicProductCategory(){
+    let productCategory: ProductCategory = {
+      id: 0,
+      category: '',
+      createdAt: new Date,
+      updatedAt: null,
+      deletedAt: null,
+      supplier: this.supplierCategoryService.inicSupplierCategory()
+    }
+
+    return productCategory;
+  }
 }
