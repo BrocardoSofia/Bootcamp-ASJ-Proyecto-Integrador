2dc7f511d0d3e5cacea259b5840b45505cef6b8d
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-08 15:41:43 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/components/product/products-create/products-create.component.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/components/product/products-create/products-create.component.ts
@@ -8,6 +8,8 @@ import { Supplier } from '../../../models/suppliers';
 import Swal from 'sweetalert2';
 import { auto } from '@popperjs/core';
 
+type SortOrder = 'None' | 'asc' | 'desc';
+
 @Component({
   selector: 'app-products-create',
   templateUrl: './products-create.component.html',
@@ -15,7 +17,25 @@ import { auto } from '@popperjs/core';
 })
 export class ProductsCreateComponent implements OnInit{
   edit: boolean = false;
-  
+
+  suppliers: Supplier[] = [];
+  currentPage: number = 0;
+  pages:number = 1;
+  maxPages: number = 5;
+  nextFive: boolean = false;
+  previous: boolean = false;
+  searchBusinessName: string = '';
+  searchSupplierCode: string = '';
+  searchBusinessNameOn: boolean = false;
+  searchSupplierCodeOn: boolean = false;
+  selectedOption: string = '1';
+  businessNameSort:SortOrder = 'None';
+  searchSupplierCategoryId: number = -1;
+  searchSupplierCategoryIdOn: boolean = false;
+
+  supplierValid:boolean = false;
+  productValid:boolean = false;
+  imagesValid:boolean = false;
 
   constructor(
     private productsService: ProductsService,
@@ -26,9 +46,93 @@ export class ProductsCreateComponent implements OnInit{
   ){}
 
   ngOnInit(): void {
-    
+    this.suppliersService.getAllActiveSuppliers(0,'','','',-1).subscribe(
+      response=>{
+        this.suppliers = response;
+      }
+    )
   }
 
+  nextPage(){
+    this.currentPage++;
+    this.selectPage(this.currentPage);
+  }
+
+  prevPage(){
+    this.currentPage--;
+    this.selectPage(this.currentPage);
+  }
+
+  searchByBusinessName(){
+    this.searchBusinessNameOn = true;
+    this.selectPage(this.currentPage);
+  }
+
+  clearSearchByBusinessName(){
+    this.searchBusinessName = '';
+    this.searchBusinessNameOn = false;
+    this.selectPage(this.currentPage);
+  }
+
+  searchBySupplierCode(){
+    this.searchSupplierCodeOn = true;
+    this.selectPage(this.currentPage);
+  }
+
+  clearSearchBySupplierCode(){
+    this.searchSupplierCode = '';
+    this.searchSupplierCodeOn = false;
+    this.selectPage(this.currentPage);
+  }
+
+  changeSort(columnName: string){
+    switch(columnName){
+      case 'businessName':
+        if(this.businessNameSort === 'None'){
+          this.businessNameSort = 'desc';
+        }else if(this.businessNameSort === 'desc'){
+          this.businessNameSort = 'asc';
+        }else{
+          this.businessNameSort = 'None';
+        }
+        break;
+    }
+    this.selectPage(0);
+  }
+
+  getSort(){
+    let sort:string = '';
+
+    if(this.businessNameSort !== 'None'){
+      sort = '&sort=businessName,'+this.businessNameSort;
+    }
+
+    return sort;
+  }
   
+  getPages(): number[] {
+    this.pages;
+    let startPage = Math.max(1, this.currentPage - Math.floor(this.maxPages / 2));
+    let endPage = Math.min(this.pages, startPage + this.maxPages - 1);
+  
+    if(this.pages > 5){
+      if(endPage-startPage != 4){
+        startPage = endPage-4;
+      }
+    }
+    let returnPages = Array.from(Array(Math.min(5, endPage - startPage + 1)), (_, i) => startPage + i);
+  
+    return returnPages;
+  }
+
+  selectPage(page: number){
+    this.suppliersService.getAllActiveSuppliers(page,this.getSort(),this.searchBusinessName,
+                            this.searchSupplierCode,this.searchSupplierCategoryId).subscribe(
+            data=>{
+              this.suppliers = data.content;
+              this.pages = data.totalPages;
+            }
+            );  
+  }
 
 }
