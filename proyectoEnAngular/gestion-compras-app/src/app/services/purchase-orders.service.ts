bd0ac37f03eef6c697e04fe5ac4819c89ce5b2b9
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-09 03:10:14 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/services/purchase-orders.service.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/services/purchase-orders.service.ts
@@ -3,13 +3,19 @@ import { PurchaseOrder } from '../models/purchase-order';
 import { SuppliersService } from './suppliers.service';
 import { Product } from '../models/product';
 import { ProductPurchase } from '../models/product-po';
+import { HttpClient } from '@angular/common/http';
+import { LoginService } from './login.service';
+import { ProductsService } from './products.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseOrdersService {
+  private url: string = 'http://localhost:8080/purchase-orders';
 
-  constructor(private suppliersService: SuppliersService) {}
+  constructor(private http: HttpClient,
+              private loginService: LoginService,
+              private productService: ProductsService) {}
 
   
 }
