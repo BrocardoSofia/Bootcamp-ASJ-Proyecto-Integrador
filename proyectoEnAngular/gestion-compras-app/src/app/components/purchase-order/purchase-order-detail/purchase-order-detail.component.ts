cd91aac20e0334b4eaddb6cf0475eaf898f17d8c
BrocardoSofia <sofia.brocardo@hotmail.com>
2024-02-09 14:38:36 -0300

    update

--- proyectoEnAngular/gestion-compras-app/src/app/components/purchase-order/purchase-order-detail/purchase-order-detail.component.ts
+++ proyectoEnAngular/gestion-compras-app/src/app/components/purchase-order/purchase-order-detail/purchase-order-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PurchaseOrder } from '../../../models/purchase-order';
 import { PurchaseOrdersService } from '../../../services/purchase-orders.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-purchase-order-detail',
@@ -9,14 +9,35 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './purchase-order-detail.component.css'
 })
 export class PurchaseOrderDetailComponent implements OnInit{
+  purchaseOrders!: PurchaseOrder;
+  idParam: number = 0;
 
   constructor(
+    private router: Router,
     private purchaseOrdersService: PurchaseOrdersService,
     private activeRoute: ActivatedRoute,
   ){}
 
   ngOnInit(): void {
-    
+    this.activeRoute.queryParamMap.subscribe((params) => {
+      let param = params.get('purchaseOrderId') || null;
+
+      if (param !== null) {
+        this.idParam = parseInt(param);
+
+        this.purchaseOrdersService.getPurchaseOrderById(this.idParam).subscribe(response => {
+
+          if(response !== null){
+            this.purchaseOrders = response;
+            
+          }else{
+            //lo redirijo a la pagina anterior
+            this.router.navigate(['/purchase-orders']);
+          }
+          
+        });
+      }
+    });
   }
 
   
